02d764cb5d6401ca3695e3c8bdbd2b088ad005a6
adam <adamardiyantow@gmail.com>
2025-08-15 14:01:41 +0700

    Refactor home view to display list of board names instead of a static message

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,13 @@
 from django.http import HttpResponse
+from .models import Board
 
 def home(request):
-    return HttpResponse("Welcome to the Boards Home Page!")
+    boards = Board.objects.all()
+    boards_name = list()
+    
+    for board in boards:
+        boards_name.append(board.name)
+
+    response_html = '<br>'.join(boards_name)
+
+    return HttpResponse(response_html)
