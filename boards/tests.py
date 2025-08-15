b274d1c9b8177fce272c04e04343599cc65ae171
adam <adamardiyantow@gmail.com>
2025-08-15 14:37:34 +0700

    Add tests for home view status code and URL resolution

--- boards/tests.py
+++ boards/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.urls import resolve
+from .views import home
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
 
-# Create your tests here.
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEqual(view.func, home)
