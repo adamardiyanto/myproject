26121c876711f4ff89c78cbd9f723a952443e9b8
adam <adamardiyantow@gmail.com>
2025-08-15 14:13:29 +0700

    Refactor home view to render a dynamic HTML template for displaying boards

--- boards/views.py
+++ boards/views.py
@@ -1,13 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Board
 
 def home(request):
     boards = Board.objects.all()
-    boards_name = list()
     
-    for board in boards:
-        boards_name.append(board.name)
-
-    response_html = '<br>'.join(boards_name)
-
-    return HttpResponse(response_html)
+    return render(request, 'home.html', {'boards': boards})
