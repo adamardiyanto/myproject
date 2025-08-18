4029d9975cec1be140e53955cb59802e04a9132c
adam <adamardiyantow@gmail.com>
2025-08-18 14:58:34 +0700

    Refactor signup view and tests to use SignUpForm for user registration

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login as auth_login
 
+from .forms import SignUpForm
 
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             user = form.save()
             auth_login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
 
