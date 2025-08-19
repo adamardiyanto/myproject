be998fa54195823f16fccc7d3e7e2bfd5ac126e8
adam <adamardiyantow@gmail.com>
2025-08-19 08:55:15 +0700

    Implement user authentication features including login and update settings for secret key management

--- myproject/settings.py
+++ myproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-62--3tnwo7!g_m4uokdnqbz_s&g#_un+xi#263ed!xq_q&uz%%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -141,3 +141,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 LOGOUT_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'home'
